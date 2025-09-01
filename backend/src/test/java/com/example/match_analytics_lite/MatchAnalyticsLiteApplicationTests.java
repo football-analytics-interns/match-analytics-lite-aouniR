c1ae2545758c1265f254e5faf717f120469a2226
AOUNI Radhouan <aouni.radhouan@outlook.com>
2025-09-01 22:41:06 +0100

    Init Spring-boot backend

---
+++ backend/src/test/java/com/example/match_analytics_lite/MatchAnalyticsLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.match_analytics_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchAnalyticsLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
