37296545c45ff7f6d10411fec57856cf7695beea
AOUNI Radhouan <aouni.radhouan@outlook.com>
2025-09-02 14:59:37 +0100

    Up+1 app.prop

--- backend/src/main/resources/db/migration/V3__Create_initial_tables.sql
+++ backend/src/main/resources/db/migration/V3__Create_initial_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE matches (
+CREATE TABLE IF NOT EXISTS "matches" (
     id SERIAL PRIMARY KEY,
     match_date TIMESTAMP NOT NULL,
     homeTeam VARCHAR(50) NOT NULL,
@@ -7,14 +7,14 @@ CREATE TABLE matches (
     awayScore INTEGER DEFAULT 0,    
 );
 
-CREATE TABLE players (
+CREATE TABLE IF NOT EXISTS "players" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     team VARCHAR(50) NOT NULL,
     position position_type NOT NULL,
 );
 
-CREATE TABLE events (
+CREATE TABLE IF NOT EXISTS "events" (
     id SERIAL PRIMARY KEY,
     match_id INTEGER NOT NULL,
     player_id INTEGER NOT NULL,
