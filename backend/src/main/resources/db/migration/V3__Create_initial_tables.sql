df21aab4b48700035bd373ecdb37cb9e53904f7a
AOUNI Radhouan <aouni.radhouan@outlook.com>
2025-09-02 09:48:51 +0100

    Up+1 db

--- backend/src/main/resources/db/migration/V3__Create_initial_tables.sql
+++ backend/src/main/resources/db/migration/V3__Create_initial_tables.sql
@@ -1,20 +1,20 @@
-CREATE TABLE match (
+CREATE TABLE matches (
     id SERIAL PRIMARY KEY,
-    date TIMESTAMP NOT NULL,
+    match_date TIMESTAMP NOT NULL,
     homeTeam VARCHAR(50) NOT NULL,
     awayTeam VARCHAR(50) NOT NULL,
     homeScore INTEGER DEFAULT 0,        
     awayScore INTEGER DEFAULT 0,    
 );
 
-CREATE TABLE player (
+CREATE TABLE players (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     team VARCHAR(50) NOT NULL,
     position position_type NOT NULL,
 );
 
-CREATE TABLE event (
+CREATE TABLE events (
     id SERIAL PRIMARY KEY,
     match_id INTEGER NOT NULL,
     player_id INTEGER NOT NULL,
